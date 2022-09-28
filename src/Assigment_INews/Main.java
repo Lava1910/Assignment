2c15588efbe838306b3851af7d783a226a8a114e
Lava1910 <lavietanh.utc@gmail.com>
2022-09-29 01:41:25 +0700

    Em nop bai tap buoi 10 a

---
+++ src/Assigment_INews/Main.java
@@ -0,0 +1,12 @@
+package Assigment_INews;
+
+public class Main {
+    public static void main(String[] args){
+        int[] arr = new int[]{2, 3, 5};
+        News a = new News(1, "abc", "22/12/2020", "Toto", "abc1");
+        a.AverageRate= a.Calculate(arr[0],
+                arr[1],
+                arr[2]);
+        a.display();
+    }
+}
